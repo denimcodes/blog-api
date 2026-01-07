f1d6cad63239aa963987fbb1aa260a0020873597
Denimsun Basumatary <denimbasumatary@outlook.com>
2026-01-07 18:26:00 +0530

    add login feature

---
+++ src/controllers/v1/login.ts
@@ -0,0 +1,74 @@
+/**
+ * @copyright 2026 Denimsun Basumatary
+ * @license Apache-2.0
+ */
+
+import User from '@/models/user';
+import Token from '@/models/token';
+
+import type { Request, Response } from 'express';
+import type { IUser } from '@/models/user';
+import { generateAccessToken, generateRefreshToken } from '@/lib/jwt';
+import { logger } from '@/lib/winston';
+import config from '@/config';
+
+type UserData = Pick<IUser, 'email' | 'password'>;
+
+const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body as UserData;
+
+  try {
+    const user = await User.findOne({
+      email,
+    })
+    .select('username email password role')
+    .lean()
+    .exec();
+    
+    if (!user) {
+      res.status(401).json({
+        code: 'NotFound',
+        message: 'User not found'
+      })
+      return;
+    }
+
+    // Generate access token and refresh token for new user
+    const accessToken = generateAccessToken(user._id);
+    const refreshToken = generateRefreshToken(user._id);
+
+    // Store refresh token in db
+    await Token.create({ token: refreshToken, userId: user._id });
+    logger.info('Refresh token created for user', {
+      userId: user._id,
+      token: refreshToken,
+    });
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: config.NODE_ENV === 'production',
+      sameSite: 'strict',
+    });
+
+    res.status(201).json({
+      user: {
+        username: user.username,
+        email: user.email,
+        role: user.role
+      },
+      accessToken,
+    });
+
+    logger.info('User registered successfully', user);
+  } catch (err) {
+    res.status(500).json({
+      code: 'ServerError',
+      message: 'Internal server error',
+      error: err,
+    });
+
+    logger.error('Error during registration', err);
+  }
+};
+
+export default login;
--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -5,10 +5,13 @@
 
 import { Router } from 'express';
 import { body } from 'express-validator';
+import bcrypt from 'bcrypt';
 
 import register from '@/controllers/v1/register';
+
 import validationError from '@/middlewares/validation_error';
 import User from '@/models/user';
+import login from '@/controllers/v1/login';
 
 const router = Router();
 
@@ -33,7 +36,7 @@ router.post(
     .notEmpty()
     .withMessage('Password is required')
     .isLength({ min: 8 })
-    .withMessage('Password must be at least 8 characters longs'),
+    .withMessage('Password must be at least 8 characters long'),
   body('role')
     .optional()
     .isString()
@@ -42,6 +45,44 @@ router.post(
     .withMessage('Role must be either admin or user'),
   validationError,
   register,
+);
+router.post(
+  '/login',
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required')
+    .isLength({ max: 50 })
+    .withMessage('Email must be less than 50 characters')
+    .isEmail()
+    .withMessage('Invalid email address')
+    .custom(async (value) => {
+      const userExists = await User.exists({ email: value });
+      if (!userExists) {
+        throw new Error('User email or password is invalid');
+      }
+    }),
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('Password is required')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long')
+    .custom(async (value, { req }) => {
+      const { email } = req.body as { email: string };
+      const user = await User.findOne({ email }).select('password').lean().exec();
+
+      if (!user) {
+        throw new Error('User email or password is invalid');
+      }
+
+      const isPasswordMatch = await bcrypt.compare(value, user.password);
+      if (!isPasswordMatch) {
+        throw new Error('User email or password is invalid');
+      }
+    }),
+  validationError,
+  login,
 );
 
 export default router;
